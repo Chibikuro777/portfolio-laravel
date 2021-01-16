30359828b619a4efde778f12debdfe05ed05eaa4
Sanae <sanae.kawasaka@gmail.com>
2021-01-16 16:22:59 +0900

    first commit

---
+++ resources/views/top.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Big+Shoulders+Stencil+Text:wght@900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+</head>
+<body>
+    <header class="container-fluid top-header-colour">
+        <div class="d-flex flex-column align-items-center justify-content-center header-title">
+            <h4>Thanks for visiting my portfolio!</h4>
+            <div class="d-flex flex-column">
+                <h1>I'm</h1>
+                <h1 class="name">Sanae Kawasaka</h1>
+            </div>
+            <h4>I design and develop websites and applications.</h4>
+        </div>
+    </header>
+
+    <div class="container-fluid top-content">
+        <div class="row">
+            <div class="col-sm text-center py-4 d-flex flex-column align-items-center">
+                <h2 class="">
+                Work Experiences
+                </h2>
+                <span style="font-size:1000%;">
+                <i class="fas fa-laptop-code"></i>
+                </span>
+            </div>
+            <h2 class="col-sm text-center py-4">
+            Gallery
+            </h2>
+            <h2 class="col-sm text-center py-4">
+            Hobby
+            </h2>
+        </div>
+    </div>
+
+
+
+    <div class="container-fluid top-footer d-flex justify-content-end align-items-center">
+        <h2 class="mr-2 mb-0">
+            Contact Me!
+        </h2>
+        <span style="font-size:250%; color:navy;">
+            <i class="fas fa-envelope-square"></i>
+        </span>
+    </div>
+
+    
+    
+</body>
+</html>
